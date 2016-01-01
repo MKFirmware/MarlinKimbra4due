1cfa29d24e7c359bfff558e34a354cd69e6924c4
MagoKimbra <magokimbra@hotmail.com>
2016-01-01 23:29:14 +0100

    Update 4.2.3 for configurator tool

--- MarlinKimbra4due/base.h
+++ MarlinKimbra4due/base.h
@@ -4,11 +4,6 @@
 #include "Arduino.h"
 #include "pins_arduino.h"
 
-// Arduino < 1.0.0 does not define this, so we need to do it ourselves
-#ifndef analogInputToDigitalPin
-  #define analogInputToDigitalPin(p) ((p) + 0xA0)
-#endif
-
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,6 +12,10 @@
 #ifdef __SAM3X8E__
   #include "HAL.h"
 #else
+  // Arduino < 1.0.0 does not define this, so we need to do it ourselves
+  #ifndef analogInputToDigitalPin
+    #define analogInputToDigitalPin(p) ((p) + 0xA0)
+  #endif
   #include <util/delay.h>
   #include <avr/eeprom.h>
   #include "fastio.h"
@@ -29,29 +28,30 @@
 #include "mechanics.h"
 
 #include "Configuration_Version.h"
-#include "Configuration_Basic.h"
 #include "Configuration_Overall.h"
 
-#if MECH(CARTESIAN)
-  #include "Configuration_Cartesian.h"
-#elif MECH(COREXY)
-  #include "Configuration_Core.h"
-#elif MECH(COREXZ)
-  #include "Configuration_Core.h"
-#elif MECH(DELTA)
-  #include "Configuration_Delta.h"
-#elif MECH(SCARA)
-  #include "Configuration_Scara.h"
+#ifndef CONFIGURATION_OVERALL_ON
+  #include "Configuration_Basic.h"
+
+  #if MECH(CARTESIAN)
+    #include "Configuration_Cartesian.h"
+  #elif MECH(COREXY)
+    #include "Configuration_Core.h"
+  #elif MECH(COREXZ)
+    #include "Configuration_Core.h"
+  #elif MECH(DELTA)
+    #include "Configuration_Delta.h"
+  #elif MECH(SCARA)
+    #include "Configuration_Scara.h"
+  #endif
+
+  #include "Configuration_Feature.h"
 #endif
-
-#include "Configuration_Feature.h"
-#include "Configuration_Overall.h"
 
 #include "language/language.h"
 #include "conditionals.h"
 #include "sanitycheck.h"
 #include "thermistortables.h"
-
 #include "comunication.h"
 
 typedef unsigned long millis_t;
