f6a51381b1ddd1e1a7182e048993e2578d4fc012
MagoKimbra <magokimbra@hotmail.com>
2016-01-14 23:04:12 +0100

    Fix SDCARD width HAL

--- MarlinKimbra4due/module/HAL.cpp
+++ MarlinKimbra4due/module/HAL.cpp
@@ -65,7 +65,91 @@ int freeMemory() {
   return free_memory;
 }
 
-#if DISABLED(DUE_SOFTWARE_SPI)
+#ifdef DUE_SOFTWARE_SPI
+  // bitbanging transfer
+  // run at ~100KHz (necessary for init)
+  uint8_t spiTransfer(uint8_t b) { // using Mode 0
+    for (int bits = 0; bits < 8; bits++) {
+      if (b & 0x80) {
+        WRITE(MOSI_PIN, HIGH);
+      }
+      else {
+        WRITE(MOSI_PIN, LOW);
+      }
+      b <<= 1;
+
+      WRITE(SCK_PIN, HIGH);
+      delayMicroseconds(5);
+
+      if (READ(MISO_PIN)) {
+        b |= 1;
+      }
+      WRITE(SCK_PIN, LOW);
+      delayMicroseconds(5);
+    }
+    return b;
+  }
+
+  void spiBegin() {
+    SET_OUTPUT(SDSS);
+    WRITE(SDSS, HIGH);
+    SET_OUTPUT(SCK_PIN);
+    SET_INPUT(MISO_PIN);
+    SET_OUTPUT(MOSI_PIN);
+  }
+
+  void spiInit(uint8_t spiClock) {
+    WRITE(SDSS, HIGH);
+    WRITE(MOSI_PIN, HIGH);
+    WRITE(SCK_PIN, LOW);
+  }
+
+  uint8_t spiReceive() {
+    WRITE(SDSS, LOW);
+    uint8_t b = spiTransfer(0xff);
+    WRITE(SDSS, HIGH);
+    return b;
+  }
+
+  void spiReadBlock(uint8_t*buf, uint16_t nbyte) {
+    if (nbyte == 0) return;
+    WRITE(SDSS, LOW);
+    for (int i = 0; i < nbyte; i++) {
+      buf[i] = spiTransfer(0xff);
+    }
+    WRITE(SDSS, HIGH);
+  }
+
+  void spiSend(uint8_t b) {
+    WRITE(SDSS, LOW);
+    uint8_t response = spiTransfer(b);
+    WRITE(SDSS, HIGH);
+  }
+
+  void spiSend(const uint8_t* buf , size_t n) {
+    uint8_t response;
+    if (n == 0) return;
+    WRITE(SDSS, LOW);
+    for (uint16_t i = 0; i < n; i++) {
+      response = spiTransfer(buf[i]);
+    }
+    WRITE(SDSS, HIGH);
+  }
+
+  void spiSendBlock(uint8_t token, const uint8_t* buf) {
+    uint8_t response;
+
+    WRITE(SDSS, LOW);
+    response = spiTransfer(token);
+
+    for (uint16_t i = 0; i < 512; i++) {
+      response = spiTransfer(buf[i]);
+    }
+    WRITE(SDSS, HIGH);
+  }
+
+#else
+
   // --------------------------------------------------------------------------
   // hardware SPI
   // --------------------------------------------------------------------------
@@ -92,79 +176,118 @@ int freeMemory() {
       // set master mode, peripheral select, fault detection
       SPI_Configure(SPI0, ID_SPI0, SPI_MR_MSTR | SPI_MR_MODFDIS | SPI_MR_PS);
       SPI_Enable(SPI0);
-     
-      #if MB(ALLIGATOR)
-        // Init Motor Fault
-        pinMode(MOTOR_FAULT_PIN, INPUT);
-        pinMode(DAC0_SYNC, OUTPUT);
-        pinMode(SPI_EEPROM1_CS, OUTPUT);
-        pinMode(SPI_EEPROM2_CS, OUTPUT);
-        pinMode(SPI_FLASH_CS, OUTPUT);
-        //pinMode( SPI_PIN, OUTPUT);
-        digitalWrite(DAC0_SYNC , HIGH);
-        digitalWrite(SPI_EEPROM1_CS, HIGH);
-        digitalWrite(SPI_EEPROM2_CS, HIGH);
-        digitalWrite(SPI_FLASH_CS, HIGH);
-        digitalWrite(SDSS , HIGH);
-        //pinMode( SPI_PIN, OUTPUT);
-      #endif//MB(ALLIGATOR)
 
+      #if MB(ALLIGATOR)
+        SET_OUTPUT(DAC0_SYNC);
+        #if EXTRUDERS > 1
+          SET_OUTPUT(DAC1_SYNC);
+          WRITE(DAC1_SYNC, HIGH);
+        #endif
+        SET_OUTPUT(SPI_EEPROM1_CS);
+        SET_OUTPUT(SPI_EEPROM2_CS);
+        SET_OUTPUT(SPI_FLASH_CS);
+        WRITE(DAC0_SYNC, HIGH);
+        WRITE(SPI_EEPROM1_CS, HIGH );
+        WRITE(SPI_EEPROM2_CS, HIGH );
+        WRITE(SPI_FLASH_CS, HIGH );
+        WRITE(SDSS, HIGH );
+      #endif // MB(ALLIGATOR)
       PIO_Configure(
-         g_APinDescription[SPI_PIN].pPort,
-         g_APinDescription[SPI_PIN].ulPinType,
-         g_APinDescription[SPI_PIN].ulPin,
-         g_APinDescription[SPI_PIN].ulPinConfiguration);
+        g_APinDescription[SPI_PIN].pPort,
+        g_APinDescription[SPI_PIN].ulPinType,
+        g_APinDescription[SPI_PIN].ulPin,
+        g_APinDescription[SPI_PIN].ulPinConfiguration);
       spiInit(1);
       spiInitMaded = true;
     }
   }
 
   void spiInit(uint8_t spiClock) {
-    if(spiInitMaded == false)
-    {
-      if(spiClock>4) spiClock = 1;
+    if(spiInitMaded == false) {
+      if(spiClock > 4) spiClock = 1;
       #if MB(ALLIGATOR)
         // Set SPI mode 1, clock, select not active after transfer, with delay between transfers  
-        SPI_ConfigureNPCS(SPI0, SPI_CHAN_DAC, SPI_CSR_CSAAT |
-          SPI_CSR_SCBR(spiDueDividors[spiClock]) | SPI_CSR_DLYBCT(1));
+        SPI_ConfigureNPCS(SPI0, SPI_CHAN_DAC,
+                          SPI_CSR_CSAAT | SPI_CSR_SCBR(spiDueDividors[spiClock]) |
+                          SPI_CSR_DLYBCT(1));
         // Set SPI mode 0, clock, select not active after transfer, with delay between transfers 
-        SPI_ConfigureNPCS(SPI0, SPI_CHAN_EEPROM1,SPI_CSR_NCPHA | SPI_CSR_CSAAT |
-          SPI_CSR_SCBR(spiDueDividors[spiClock]) | SPI_CSR_DLYBCT(1));
+        SPI_ConfigureNPCS(SPI0, SPI_CHAN_EEPROM1, SPI_CSR_NCPHA |
+                          SPI_CSR_CSAAT | SPI_CSR_SCBR(spiDueDividors[spiClock]) |
+                          SPI_CSR_DLYBCT(1));
       #endif//MB(ALLIGATOR)
 
       // Set SPI mode 0, clock, select not active after transfer, with delay between transfers
-      SPI_ConfigureNPCS(SPI0, SPI_CHAN, SPI_CSR_NCPHA | SPI_CSR_CSAAT |
-        SPI_CSR_SCBR(spiDueDividors[spiClock]) | SPI_CSR_DLYBCT(1));
+      SPI_ConfigureNPCS(SPI0, SPI_CHAN, SPI_CSR_NCPHA |
+                        SPI_CSR_CSAAT | SPI_CSR_SCBR(spiDueDividors[spiClock]) |
+                        SPI_CSR_DLYBCT(1));
       SPI_Enable(SPI0);
       spiInitMaded = true;
     }
   }
 
-  void spiSendByte(uint32_t chan, byte b) {
+  // Write single byte to SPI
+  void spiSend(byte b) {
+    // write byte with address and end transmission flag
+    SPI0->SPI_TDR = (uint32_t)b | SPI_PCS(SPI_CHAN) | SPI_TDR_LASTXFER;
+    // wait for transmit register empty
+    while ((SPI0->SPI_SR & SPI_SR_TDRE) == 0);
+    // wait for receive register
+    while ((SPI0->SPI_SR & SPI_SR_RDRF) == 0);
+    // clear status
+    SPI0->SPI_RDR;
+    //delayMicroseconds(1);
+  }
+
+  void spiSend(const uint8_t* buf, size_t n) {
+    if (n == 0) return;
+    for (size_t i = 0; i < n - 1; i++) {
+      SPI0->SPI_TDR = (uint32_t)buf[i] | SPI_PCS(SPI_CHAN);
+      while ((SPI0->SPI_SR & SPI_SR_TDRE) == 0);
+      while ((SPI0->SPI_SR & SPI_SR_RDRF) == 0);
+      SPI0->SPI_RDR;
+      //        delayMicroseconds(1);
+    }
+    spiSend(buf[n - 1]);
+  }
+
+  void spiSend(uint32_t chan, byte b) {
     uint8_t dummy_read = 0;
     // wait for transmit register empty
     while ((SPI0->SPI_SR & SPI_SR_TDRE) == 0);
     // write byte with address and end transmission flag
     SPI0->SPI_TDR = (uint32_t)b | SPI_PCS(chan) | SPI_TDR_LASTXFER;
-    // wait for receive register 
+    // wait for receive register
     while ((SPI0->SPI_SR & SPI_SR_RDRF) == 0);
     // clear status
     while ((SPI0->SPI_SR & SPI_SR_RDRF) == 1)
       dummy_read = SPI0->SPI_RDR;
   }
 
-  void spiSend(uint32_t chan ,const uint8_t* buf , size_t n) {
+  void spiSend(uint32_t chan, const uint8_t* buf, size_t n) {
     uint8_t dummy_read = 0;
     if (n == 0) return;
-    for (int i=0; i<n-1; i++)
-    {
+    for (int i = 0; i < n - 1; i++) {
       while ((SPI0->SPI_SR & SPI_SR_TDRE) == 0);
       SPI0->SPI_TDR = (uint32_t)buf[i] | SPI_PCS(chan);
       while ((SPI0->SPI_SR & SPI_SR_RDRF) == 0);
-       while ((SPI0->SPI_SR & SPI_SR_RDRF) == 1)
+      while ((SPI0->SPI_SR & SPI_SR_RDRF) == 1)
         dummy_read = SPI0->SPI_RDR;
     }
-    spiSendByte(chan, buf[n-1]);
+    spiSend(chan, buf[n - 1]);
+  }
+
+  // Read single byte from SPI
+  uint8_t spiReceive() {
+    // write dummy byte with address and end transmission flag
+    SPI0->SPI_TDR = 0x000000FF | SPI_PCS(SPI_CHAN) | SPI_TDR_LASTXFER;
+    // wait for transmit register empty
+    while ((SPI0->SPI_SR & SPI_SR_TDRE) == 0);
+
+    // wait for receive register
+    while ((SPI0->SPI_SR & SPI_SR_RDRF) == 0);
+    // get byte from receive register
+    //delayMicroseconds(1);
+    return SPI0->SPI_RDR;
   }
 
   uint8_t spiReceive(uint32_t chan) {
@@ -177,11 +300,42 @@ int freeMemory() {
     // write dummy byte with address and end transmission flag
     SPI0->SPI_TDR = 0x000000FF | SPI_PCS(chan) | SPI_TDR_LASTXFER;
 
-    // wait for receive register 
+    // wait for receive register
     while ((SPI0->SPI_SR & SPI_SR_RDRF) == 0);
     // get byte from receive register
     return SPI0->SPI_RDR;
   }
+
+  // Read from SPI into buffer
+  void spiReadBlock(uint8_t*buf, uint16_t nbyte) {
+    if (nbyte-- == 0) return;
+
+    for (int i = 0; i < nbyte; i++) {
+      //while ((SPI0->SPI_SR & SPI_SR_TDRE) == 0);
+      SPI0->SPI_TDR = 0x000000FF | SPI_PCS(SPI_CHAN);
+      while ((SPI0->SPI_SR & SPI_SR_RDRF) == 0);
+      buf[i] = SPI0->SPI_RDR;
+      // delayMicroseconds(1);
+    }
+    buf[nbyte] = spiReceive();
+  }
+
+  // Write from buffer to SPI
+  void spiSendBlock(uint8_t token, const uint8_t* buf) {
+    SPI0->SPI_TDR = (uint32_t)token | SPI_PCS(SPI_CHAN);
+    while ((SPI0->SPI_SR & SPI_SR_TDRE) == 0);
+    //while ((SPI0->SPI_SR & SPI_SR_RDRF) == 0);
+    //SPI0->SPI_RDR;
+    for (int i = 0; i < 511; i++) {
+      SPI0->SPI_TDR = (uint32_t)buf[i] | SPI_PCS(SPI_CHAN);
+      while ((SPI0->SPI_SR & SPI_SR_TDRE) == 0);
+      while ((SPI0->SPI_SR & SPI_SR_RDRF) == 0);
+      SPI0->SPI_RDR;
+      //        delayMicroseconds(1);
+    }
+    spiSend(buf[511]);
+  }
+
 #endif // DISABLED(DUE_SOFTWARE_SPI)
 
 // --------------------------------------------------------------------------
@@ -207,20 +361,20 @@ static void eeprom_init(void) {
 
     /*write enable*/
     eeprom_temp[0] = 6;//WREN
-    digitalWrite( SPI_EEPROM1_CS , LOW );
-    spiSend(SPI_CHAN_EEPROM1 ,eeprom_temp , 1);
-    digitalWrite( SPI_EEPROM1_CS , HIGH );
+    digitalWrite( SPI_EEPROM1_CS, LOW );
+    spiSend(SPI_CHAN_EEPROM1, eeprom_temp , 1);
+    digitalWrite(SPI_EEPROM1_CS, HIGH);
     _delay_ms(1);
 
     /*write addr*/
     eeprom_temp[0] = 2;//WRITE
     eeprom_temp[1] = ((pos>>8) & 0xFF);//addrH
     eeprom_temp[2] = (pos& 0xFF);//addrL
-    digitalWrite( SPI_EEPROM1_CS , LOW );
-    spiSend(SPI_CHAN_EEPROM1 ,eeprom_temp , 3);        
+    digitalWrite(SPI_EEPROM1_CS, LOW);
+    spiSend(SPI_CHAN_EEPROM1, eeprom_temp, 3);        
 
     spiSend(SPI_CHAN_EEPROM1 ,newvalue , 1);
-    digitalWrite( SPI_EEPROM1_CS , HIGH );
+    digitalWrite(SPI_EEPROM1_CS, HIGH);
     _delay_ms(7);   // wait for page write to complete
   }
 
@@ -235,12 +389,12 @@ static void eeprom_init(void) {
     eeprom_temp[0] = 3;//READ
     eeprom_temp[1] = ((pos>>8) & 0xFF);//addrH
     eeprom_temp[2] = (pos& 0xFF);//addrL
-    digitalWrite( SPI_EEPROM1_CS , HIGH );
-    digitalWrite( SPI_EEPROM1_CS , LOW );
-    spiSend(SPI_CHAN_EEPROM1 ,eeprom_temp , 3);
+    digitalWrite(SPI_EEPROM1_CS, HIGH);
+    digitalWrite(SPI_EEPROM1_CS, LOW);
+    spiSend(SPI_CHAN_EEPROM1, eeprom_temp, 3);
 
     v = spiReceive(SPI_CHAN_EEPROM1); 
-    digitalWrite( SPI_EEPROM1_CS , HIGH );
+    digitalWrite(SPI_EEPROM1_CS, HIGH);
     return v;
   }
 #endif
@@ -443,8 +597,7 @@ HAL_BEEPER_TIMER_ISR {
 //
 // --------------------------------------------------------------------------
 
-uint16_t getAdcReading(adc_channel_num_t chan)
-{
+uint16_t getAdcReading(adc_channel_num_t chan) {
   if ((ADC->ADC_ISR & BIT(chan)) == BIT(chan)) {
     uint16_t rslt = ADC->ADC_CDR[chan];
     ADC->ADC_CHDR |= BIT(chan);
@@ -456,23 +609,18 @@ uint16_t getAdcReading(adc_channel_num_t chan)
   }
 }
 
-void startAdcConversion(adc_channel_num_t chan)
-{
+void startAdcConversion(adc_channel_num_t chan) {
   ADC->ADC_CHER |= BIT(chan);
 }
 
 // Convert an Arduino Due pin number to the corresponding ADC channel number
-adc_channel_num_t pinToAdcChannel(int pin)
-{
+adc_channel_num_t pinToAdcChannel(int pin) {
   if (pin < A0)
-  {
     pin += A0;
-  }
   return (adc_channel_num_t) (int) g_APinDescription[pin].ulADCChannelNumber;
 }
 
-uint16_t getAdcFreerun(adc_channel_num_t chan, bool wait_for_conversion)
-{
+uint16_t getAdcFreerun(adc_channel_num_t chan, bool wait_for_conversion) {
   if (wait_for_conversion) while (!((ADC->ADC_ISR & BIT(chan)) == BIT(chan)));
   if ((ADC->ADC_ISR & BIT(chan)) == BIT(chan)) {
     uint16_t rslt = ADC->ADC_CDR[chan];
@@ -490,14 +638,6 @@ uint16_t getAdcSuperSample(adc_channel_num_t chan) {
   return rslt /4 ;
 }
 
-void stopAdcFreerun(adc_channel_num_t chan)
-{
+void stopAdcFreerun(adc_channel_num_t chan) {
   ADC->ADC_CHDR |= BIT(chan);
 }
-
-// --------------------------------------------------------------------------
-//! @brief
-//! @param[in]
-//! @param[out]
-//! @return
-// --------------------------------------------------------------------------
