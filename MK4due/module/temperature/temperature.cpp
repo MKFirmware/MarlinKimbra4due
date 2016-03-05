69bdca31c215b823d29d63bd1fbecac00cd844f0
Giuseppe Treccarichi <giutrec@gmail.com>
2016-03-05 10:20:48 +0100

    Fixed original implementation of MAX6675

--- MK4due/module/temperature/temperature.cpp
+++ MK4due/module/temperature/temperature.cpp
@@ -1218,15 +1218,17 @@ void disable_all_heaters() {
   static int16_t max6675_temp = 0;
 
   int16_t read_max6675() {
+    if (HAL::timeInMilliseconds() - last_max6675_read > 230) {
       HAL::spiBegin();
       HAL::spiInit(2);
-      HAL::digitalWrite(MAX6675_SS, 0);  // enable TT_MAX6675
-      HAL::delayMilliseconds(230); // wait 230ms till result in register
-      HAL::delayMicroseconds(1); // ensure 100ns delay - a bit extra is fine
-      max6675_temp = HAL::spiReceive();
+      HAL::digitalWrite(MAX6675_SS, 0); // enable TT_MAX6675
+      HAL::delayMicroseconds(1);    // ensure 100ns delay - a bit extra is fine
+      max6675_temp = HAL::spiReceive(); //read higher byte
       max6675_temp <<= 8;
-      max6675_temp |= HAL::spiReceive();
-      HAL::digitalWrite(MAX6675_SS, 1); // disable TT_MAX6675
+      max6675_temp |= HAL::spiReceive(); //read lower byte
+      HAL::digitalWrite(MAX6675_SS, 1);  // disable TT_MAX6675
+      last_max6675_read = HAL::timeInMilliseconds();
+    } 
     return max6675_temp & 4 ? 2000 : max6675_temp >> 3; // thermocouple open?
   }
 #endif // HEATER_0_USES_MAX6675
