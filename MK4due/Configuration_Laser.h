6d077bb6927dcb769cea6259dd437aa78bb1faa6
MagoKimbra <magokimbra@hotmail.com>
2016-06-05 09:06:42 +0200

    Fix

--- MK4due/Configuration_Laser.h
+++ MK4due/Configuration_Laser.h
@@ -11,6 +11,12 @@
 // 2 = Two pin control - A firing pin for which LOW = off, HIGH = on, and a seperate intensity pin which carries a constant PWM signal and adjusts duty cycle to control intensity
 #define LASER_CONTROL 1
 
+// The following define to use the new HakanBasted laser_pulse method to fire laser. It should be more efficient, but it's less tested.
+// Thanks for it to HakanBastedt that has implemented it for Marlin at https://github.com/HakanBastedt/Marlin
+// Uncomment to enable it *USE AT YOUR OWN RISK*, it should work but it's *NOT WELL TESTED YET*
+// Only for MEGA. On DUE processor is automatic.
+//#define LASER_PULSE_METHOD
+
 // If your machine has laser focuser, set this to true and it will use Z axis for focus or disable it.
 #define LASER_HAS_FOCUS false
 
